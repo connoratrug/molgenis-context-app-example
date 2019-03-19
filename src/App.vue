78034555e20627940f4a3a01b064715a231858ef
connor <c.stroomberg@gmail.com>
2019-03-19 18:00:34 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
   <div id="app">
-    <NavBar v-if="showNav" :molgenis-menu="molgenisMenu"/>
+    <NavBar v-if="isContextLoaded" :molgenis-menu="molgenisMenu"/>
     <div class="container">
       <HelloWorld/>
     </div>
+    <FooterComponent v-if="isContextLoaded" :molgenisFooter="footerData"/>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 import api from '@molgenis/molgenis-api-client'
 import NavBar from '@molgenis/molgenis-ui-context/src/components/NavBar'
+import FooterComponent from '@molgenis/molgenis-ui-context/src/components/FooterComponent'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld, NavBar
+    HelloWorld, NavBar, FooterComponent
   },
   data () {
     return {
       molgenisMenu: {},
-      showNav: false
+      footerData: {},
+      isContextLoaded: false
     }
   },
   mounted () {
     api.get('/plugin/app-ui-context').then((context) => {
       this.molgenisMenu = context
-      this.showNav = true
+      this.footerData = context
+      this.isContextLoaded = true
     })
   }
 })
